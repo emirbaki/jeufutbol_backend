2504b276615180d7710a5988a801b64221f98b56
emirbaki <emirbaki.068@gmail.com>
2025-10-28 17:17:15 +0300

    origin_for_all

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
-    // origin: process.env.FRONTEND_URL || 'http://localhost:4200',
+    origin: '*', //process.env.FRONTEND_URL || 'http://localhost:4200',
     credentials: true,
   });
   app.setGlobalPrefix('api');
