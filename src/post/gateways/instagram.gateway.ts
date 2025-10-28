670a497cc02b75a88b85e765a221aaf57d4aad2c
emirbaki <emirbaki.068@gmail.com>
2025-10-29 01:57:40 +0300

    number

--- src/post/gateways/instagram.gateway.ts
+++ src/post/gateways/instagram.gateway.ts
@@ -94,7 +94,7 @@ export class InstagramPostGateway implements PostGateway {
             `${GRAPH_API_BASE}/${accountID}/media_publish`,
             {
               params: {
-                creation_id: containerIds.join(','),
+                creation_id: containerIds.join(',') as unknown as number,
                 caption: content,
               },
             },
