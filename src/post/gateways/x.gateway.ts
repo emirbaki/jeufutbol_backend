be3056c2c009c89fbd50ac6a5d7d59e2e1e48297
emirbaki <emirbaki.068@gmail.com>
2025-11-12 00:39:54 +0300

    Denemeeeee

--- src/post/gateways/x.gateway.ts
+++ src/post/gateways/x.gateway.ts
@@ -42,7 +42,7 @@ export class XPostGateway implements PostGateway {
       this.twitterClient = new TwitterApi({
         appKey: process.env.X_API_KEY!,
         appSecret: process.env.X_API_SECRET!,
-        accessToken: process.env.X_ACCESS_TOKEN,
+        accessToken: access_token,
         accessSecret: process.env.X_ACCESS_TOKEN_SECRET,
       });
       const uploadStrings: string[] = [];
