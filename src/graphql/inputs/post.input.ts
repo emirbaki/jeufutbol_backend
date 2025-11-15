70df9027715185c19e4ef043949ad880e7454b00
emirbaki <emirbaki.068@gmail.com>
2025-11-16 02:55:27 +0300

    update-post.input

--- src/graphql/inputs/post.input.ts
+++ src/graphql/inputs/post.input.ts
@@ -1,6 +1,6 @@
 import { InputType, Field } from '@nestjs/graphql';
 import { IsISO8601, IsOptional } from 'class-validator';
-import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json';
+import GraphQLJSON from 'graphql-type-json';
 
 @InputType()
 export class CreatePostInput {
@@ -20,22 +20,4 @@ export class CreatePostInput {
   @IsISO8601()
   @Field({ nullable: true })
   scheduledFor: string | null;
-}
-
-@InputType()
-export class UpdatePostInput {
-  @Field({ nullable: true })
-  content?: string;
-
-  @Field(() => [String], { nullable: true })
-  mediaUrls?: string[];
-
-  @Field(() => GraphQLJSONObject, { nullable: true })
-  platformSpecificContent?: Record<string, any>;
-
-  @Field(() => [String], { nullable: true })
-  targetPlatforms?: string[];
-
-  @Field({ nullable: true })
-  scheduledFor?: Date;
 }
