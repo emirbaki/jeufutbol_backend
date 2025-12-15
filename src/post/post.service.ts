0da1627a3264c744b7fa7d76a16594c6c105d209
emirbaki <emirbaki.068@gmail.com>
2025-12-15 22:35:45 +0300

    mini update on fixation view link on shorts stuff

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -296,6 +296,7 @@ export class PostsService {
               {
                 username: credential.accountName,
                 mediaUrls: post.mediaUrls,
+                youtubeSettings: gatewayOptions.youtubeSettings,
               },
             );
 
